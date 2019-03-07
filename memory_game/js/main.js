ac7fd8f9986a3bc8d5db09326d3d1ada1bc79629
Sammi Bachir <sammibachir@Sammis-MacBook-Pro.local>
2019-03-07 14:01:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,43 +22,37 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-        } else {
-        console.log("Sorry, try again.");
-        }
+        alert('You found a match!');
+        } 
+    else {
+        alert('Sorry, try again.');
+    }
 };
+
 var flipCard = function() {
     var cardId = this.getAttribute('data-id');
-    cardsInPlay.push(cards[cardId].rank);    
-    console.log("User flipped " + cards[cardId].rank);
+    this.setAttribute("src", cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);   
+if (cardsInPlay.length === 2) {
     checkForMatch();
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-};
-
-document.getElementsByTagName('data-id')[0].setAttribute('card-img', cards.cardImage);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
 }
-else {
-    alert("Sorry, try again.");
 };
+
 
     var createBoard = function() {
-    for (var i = 0; i < cardsInPlay.length; i++) {
-        var cardElement = [ 
-            document.createElement('img'),
-            document.createElement('img'),
-            document.createElement('img'),
-            document.createElement('img')
-        ];
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
         cardElement.setAttribute('src', 'images/back.png');
         cardElement.setAttribute('data-id', i);
         cardElement.addEventListener('click', flipCard);
-        document.getElementById('game-board')[0].appendChild(cardElement);
-    };
-    createBoard();
-}
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+};
+createBoard();
+
+function myButton() {
+    location.reload();
+};
